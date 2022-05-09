9f98ab6b64063d6228a12f822e93b136ef18d5c5
VGeorgiev <vgeorgiev@up2technology.com>
2022-05-09 23:45:46 +0300

    feat: Add new data models to the amlify backend

--- src/models/index.js
+++ src/models/index.js
@@ -2,11 +2,22 @@
 import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
 
+const OrderStatus = {
+  "NEW": "NEW",
+  "COOKING": "COOKING",
+  "READY_FOR_PICKUP": "READY_FOR_PICKUP",
+  "COMPLETE": "COMPLETE"
+};
 
-
-const { Dish, Restaurant } = initSchema(schema);
+const { Basket, BasketDish, Dish, OrderDish, Order, Restaurant, User } = initSchema(schema);
 
 export {
+  Basket,
+  BasketDish,
   Dish,
-  Restaurant
+  OrderDish,
+  Order,
+  Restaurant,
+  User,
+  OrderStatus
 };
