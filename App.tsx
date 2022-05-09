5c2ccf1c4c20ddbe0699e3d4156985f122848167
VGeorgiev <vgeorgiev@up2technology.com>
2022-05-09 14:21:01 +0300

    feat: amplify init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import RootNavigator from './src/navigation/MainNavigation';
 // import Loading from './src/components/Loading';
 import Navigator from './src/navigation/Navigator';
 import { RecoilRoot } from 'recoil';
 import { LogBox } from 'react-native';
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+Amplify.configure(awsconfig);
 
 // TMP Fix for Recoil using a long timer for cleanup
 LogBox.ignoreLogs(['timer']);
 
-import { NavigationContainer } from '@react-navigation/native';
-export default function App() {
+const App: React.FC = () => {
   return (
     <RecoilRoot>
       <StatusBar />
       <Navigator />
     </RecoilRoot>
   );
-}
+};
+
+export default withAuthenticator(App);
